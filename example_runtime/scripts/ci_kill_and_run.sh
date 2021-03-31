ab786a36ca9c9505e3133206d0bacab0eb70ef98
Guillaume G Noell <gn5@sanger.ac.uk>
2021-03-31 11:18:42 +0100

    push

--- example_runtime/scripts/ci_kill_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git add * && \
-  git add .gitlab-ci.yml && \
-  git add .gitignore && \
-  git commit --allow-empty -m "auto_gitlab; ci_run_kill; ci_run_lsf" && \
-  git push -u origin
