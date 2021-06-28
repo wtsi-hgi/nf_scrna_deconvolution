5d47ae87b3e48ec9d03f9681fc66996f54669064
Guillaume G Noell <gn5@sanger.ac.uk>
2021-06-28 13:29:56 +0100

    auto-git

---
+++ inputs/get_souporcell_ref.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# cf. https://github.com/wheaton5/souporcell
+wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
+tar -xzvf refdata-cellranger-GRCh38-3.0.0.tar.gz
