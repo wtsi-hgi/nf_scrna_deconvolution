b2de1c7940a233d432eda795391b3d99003cf0fd
Guillaume G Noell <gn5@sanger.ac.uk>
2021-06-28 13:37:24 +0100

    auto-git

--- inputs/get_souporcell_ref.sh
+++ inputs/get_souporcell_ref.sh
@@ -3,3 +3,6 @@
 # cf. https://github.com/wheaton5/souporcell
 wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
 tar -xzvf refdata-cellranger-GRCh38-3.0.0.tar.gz
+
+# this will create folder refdata-cellranger-GRCh38-3.0.0 in current dir
+# Sanger HGI scRNA: run at /lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/pipeline_inputs/deconv/nf_scrna_deconvolution/inputs
